fb758ba72b2e2c91d109f4107eee4d44dea169c7
JasenLassig <lassig.jasen@gmail.com>
2019-11-09 15:43:29 -0700

    Model First Draft

---
+++ MemeCo/Models/Like.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeCo.Models
+{
+    public class Like
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        [Required]
+        public Guid PostId { get; set; }
+        [Required]
+        public bool Liked { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime TimeLiked { get; set; }
+    }
+}
---
+++ MemeCo/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemeCo.Models
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        public byte[] Meme { get; set; }
+        public string Description { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime TimePosted { get; set; }
+        public IEnumerable<Like> Likes { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
