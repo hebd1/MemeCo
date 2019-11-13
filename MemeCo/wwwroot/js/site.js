9b6a5b1de8776f1bb5b414a8b63ea19416990768
JoseMonterroso <jmonterroso6@icloud.com>
2019-11-13 13:03:18 -0700

    dark mode

--- MemeCo/wwwroot/js/site.js
+++ MemeCo/wwwroot/js/site.js
@@ -2,11 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-
-function myFunction()
-{
-
-    document.getElementById("Logo").src = "/files/DarkLogo.png";    
-    document.getElementById("css").href = "/css/dark-mode.css";
-}
