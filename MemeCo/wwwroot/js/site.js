c0d99c6cdff2660d17082bd9b7c93889448c236e
JoseMonterroso <jmonterroso6@icloud.com>
2019-11-12 20:42:42 -0700

    Added Logo instead of text. Added Light/Dark mode

--- MemeCo/wwwroot/js/site.js
+++ MemeCo/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+function myFunction()
+{
+
+    document.getElementById("Logo").src = "/files/DarkLogo.png";    
+    document.getElementById("css").href = "/css/dark-mode.css";
+}
