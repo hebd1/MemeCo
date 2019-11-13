86dbe0a1ab56f891c46d002eea5139b96ef08bde
hebd1 <elihebdon@gmail.com>
2019-11-12 18:38:50 -0700

    Added DBInitializer class. Currently adds single user and post to DB. Changed Post Timestamp property to be autogenerated when initialized.

--- MemeCo/Program.cs
+++ MemeCo/Program.cs
@@ -1,26 +1,46 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using MemeCo.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using MemeCo.Models;
+using MemeCo;
 
-namespace MemeCo
+namespace Learning_Outcome_Tracker
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            var services = host.Services.CreateScope().ServiceProvider;
+            try
+            {
+                var userManager = services.GetRequiredService<UserManager<MemeCoUser>>();
+                var _memeCoContext = services.GetRequiredService<MemeCoContext>();
+                DbInitializer initializer = new DbInitializer(userManager, _memeCoContext);
+                initializer.Seed();
+
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred while seeding the database.");
+            }
+
+
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
