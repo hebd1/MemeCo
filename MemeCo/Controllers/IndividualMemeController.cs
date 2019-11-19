f436c6ae9aaf89bfd3ca29306cd96d9cf9deda8a
JoseMonterroso <jmonterroso6@icloud.com>
2019-11-19 10:03:04 -0700

    Added the individual meme page

---
+++ MemeCo/Controllers/IndividualMemeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using MemeCo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemeCo.Controllers
+{
+    public class IndividualMemeController : Controller
+    {
+        private MemeCoContext _context;
+
+        public IndividualMemeController(MemeCoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("/{postID}")]
+        public async Task<IActionResult> Index(Guid postID)
+        {
+            var post = await _context.Posts.Include(o => o.Comments).ThenInclude(i => i.User).FirstOrDefaultAsync(p => p.ID == postID);
+            return View(post);
+        }
+    }
+}
