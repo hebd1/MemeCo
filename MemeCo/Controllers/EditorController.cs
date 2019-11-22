85f3d7c6b20ded48a24af21cfae5639b7ee6eac1
hebd1 <elihebdon@gmail.com>
2019-11-21 23:59:55 -0700

    Made some progress with the meme editor

--- MemeCo/Controllers/EditorController.cs
+++ MemeCo/Controllers/EditorController.cs
@@ -6,23 +6,91 @@ using Microsoft.AspNetCore.Mvc;
 using MemeCo.Areas.Identity.Data;
 using MemeCo.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace MemeCo.Controllers
 {
 
     public class EditorController : Controller
     {
-        private MemeCoContext _context;
+        private readonly MemeCoContext _context;
+        private readonly UserManager<MemeCoUser> _user_manager;
 
 
-        public EditorController(MemeCoContext context)
+        public EditorController(MemeCoContext context, UserManager<MemeCoUser> user_manager)
         {
             _context = context;
+            _user_manager = user_manager;
         }
+
+
         public IActionResult Index()
         {
             var result = _context.Posts.Include(o => o.Likes).Include(o => o.User).ThenInclude(o => o.Followers).Include(o => o.Comments).ToList();
             return View(result);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Post_Meme(string user_id, string description, string image, int template_id)
+        {
+          
+
+            try
+            {
+                var user = await _user_manager.FindByIdAsync(user_id);
+                var img = System.IO.File.ReadAllBytes(image);
+
+                //byte[] imageArr;
+                //if (image != null)
+                //{
+                //    using (var ms = new MemoryStream())
+                //    {
+                //        image.CopyTo(ms);
+                //        imageArr = ms.ToArray();
+                //    }
+                //}
+
+                // save image array to DB
+
+
+                // not logged in
+                if (user == null)
+                {
+                    return Json(
+                   new
+                   {
+                       success = false,
+                       user_id = user_id,
+                       description = description,
+                       image = image,
+                       template_id = template_id
+
+                   });
+                }
+                else
+                {
+                   
+
+                    // success
+                    return Json(
+                        new
+                        {
+                            success = true,
+                         
+                        });
+                }
+                // something else went wrong
+            }
+            catch (Exception)
+            {
+                return Json(
+                   new
+                   {
+                       success = false,
+                       
+                   });
+            }
         }
     }
 }
---
+++ MemeCo/wwwroot/js/postMeme.js
@@ -0,0 +1,42 @@
+﻿function post_meme(ths, user_id, template_id, e) {
+    e.preventDefault();
+    console.log("post meme reached");
+    var meme = $('#meme-content').attr('src');
+   
+    var caption = $('#caption-text').text();
+    $.ajax({
+
+        method: "POST",
+        url: "/Editor/Post_Meme",
+        data:
+        {
+            user_id: user_id,
+            description: caption,
+            image: meme,
+            template_id: template_id
+        }
+    }).done(function (result) {
+
+        if (result.success == false) {
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'You have to login before you can get to memeing',
+                timer: 3000
+            })
+        } else {
+
+            
+        }
+
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        console.log("fail");
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Server Error Please try again later',
+            timer: 2000
+        })
+    }).always(function () { });
+
+}
