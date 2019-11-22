203c3b0479e3ed5a95d3e4eb8f3445ef1e5b3d1e
hebd1 <elihebdon@gmail.com>
2019-11-21 17:06:46 -0700

    Added meme editor page

---
+++ MemeCo/Controllers/EditorController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MemeCo.Areas.Identity.Data;
+using MemeCo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemeCo.Controllers
+{
+
+    public class EditorController : Controller
+    {
+        private MemeCoContext _context;
+
+
+        public EditorController(MemeCoContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Posts.Include(o => o.Likes).Include(o => o.User).ThenInclude(o => o.Followers).Include(o => o.Comments).ToList();
+            return View(result);
+        }
+    }
+}
