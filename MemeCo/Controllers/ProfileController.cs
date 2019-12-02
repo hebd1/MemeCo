7cd0b283471476e72eaea3d5129d5fa6525634b5
JasenLassig <lassig.jasen@gmail.com>
2019-12-02 11:25:31 -0700

    profile header first draft

--- MemeCo/Controllers/ProfileController.cs
+++ MemeCo/Controllers/ProfileController.cs
@@ -21,7 +21,13 @@ namespace MemeCo.Controllers
         [HttpGet("/{username}")]
         public async Task<IActionResult> Index(string username)
         {
-            var user = await _userManager.FindByNameAsync(username);
+            MemeCoUser user = await _userManager.FindByNameAsync(username);
+
+            user.Followers = _context.Follows.Where(x => x.UserID == user.Id).OrderBy(x => x.Follower.UserName).ToList();
+            user.Following = _context.Follows.Where(x => x.FollowerID == user.Id).OrderBy(x => x.User.UserName).ToList();
+            user.Likes = _context.Likes.Where(x => x.MemeCoUserID == user.Id).OrderBy(x => x.TimeLiked.Millisecond).ToList();
+            user.Posts = _context.Posts.Where(x => x.MemeCoUserID == user.Id).OrderBy(x => x.TimePosted.Millisecond).ToList();
+            user.Comments = _context.Comments.Where(x => x.MemeCoUserID == user.Id).OrderBy(x => x.TimeCommented.Millisecond).ToList();
 
             return View(user);
         }
